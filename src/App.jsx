1c3008adbe46d5018ffee4ae5b1219f0f49986ca
StackOverfloweds <bryangithub32@gmail.com>
2025-03-22 15:59:12 +0700

    update: add product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Product from "./pages/Product/Product";
 import About from "./pages/About/About";
 import Login from "./pages/Auth/Login";
-import AppHome from "./pages/AppHome";
+import Home from "./pages/Home/Home";
+import HeaderSlider from "./components/HeaderSlider";
+import LoadingComponent from "./components/LoadingComponent";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading process, then hide the loading component
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <Router>
-      <Navbar />
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<AppHome />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      {loading ? (
+        <LoadingComponent />
+      ) : (
+        <Router>
+          <Navbar />
+          <div className="container mt-5">
+            <Routes>
+              <Route path="/" element={<><HeaderSlider /><Home /></>} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </Router>
+      )}
+    </>
   );
 }
 
--- src/api/smartphoneAPI.js
+++ src/api/smartphoneAPI.js
@@ -9,3 +9,14 @@ export const getSmartphones = async () => {
     return [];
   }
 };
+
+// Fetch smartphone details by ID
+export const getSmartphoneByID = async (id) => {
+  try {
+    const response = await axiosClient.get(`/smartphone/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching smartphone with ID ${id}:`, error);
+    return null;
+  }
+};
